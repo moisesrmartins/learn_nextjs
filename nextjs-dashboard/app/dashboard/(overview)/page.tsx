6ac8132d8cc33a8f7104793e157a0729b647aa47
Moisés Rodrigues Martins <moisesmartins.box@gmail.com>
2024-05-12 10:27:13 -0300

    Parte 9.4: Streaming do LatestInvoices

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -2,12 +2,11 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+import { fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { RevenueChartSkeleton, LatestInvoicesSkeleton } from '@/app/ui/skeletons';
 
 export default async function Page() {
-    const latestInvoices = await fetchLatestInvoices();
     const {
         numberOfInvoices,
         numberOfCustomers,
@@ -28,8 +27,11 @@ export default async function Page() {
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
                 <Suspense fallback={<RevenueChartSkeleton />}>
                     <RevenueChart />
+                    <LatestInvoices />
                 </Suspense>
-                {<LatestInvoices latestInvoices={latestInvoices} />}
+                <Suspense fallback={<LatestInvoicesSkeleton />}>
+                    <LatestInvoices />
+                </Suspense>
             </div>
         </main>
     );
