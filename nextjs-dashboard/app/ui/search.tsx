8d91e7f807908d24327131baf8d6e4c5df97b41e
Moisés Rodrigues Martins <moisesmartins.box@gmail.com>
2024-05-18 20:42:03 -0300

    Parte 11.3: Atualizxando o URL com os parâmetros de pesquisa

--- nextjs-dashboard/app/ui/search.tsx
+++ nextjs-dashboard/app/ui/search.tsx
@@ -1,10 +1,21 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
-export default function Search({ placeholder }: { placeholder: string }) {
+export default function Search() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
   function handleSearch(term: string) {
-    console.log(term);
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
   }
 
   return (
@@ -14,7 +25,6 @@ export default function Search({ placeholder }: { placeholder: string }) {
       </label>
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        placeholder={placeholder}
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
